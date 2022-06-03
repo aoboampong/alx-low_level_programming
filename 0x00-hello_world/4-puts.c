01c7f9b8dc7bcfa39af2fe5db426c36be68e8e57
boampsremarks <boampsremarks@gmail.com>
2022-06-02 18:32:42 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* Imported the Standard input and output library */
+
+
+/* main body */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+
+/* returns 0 showing success */
+	return (0);
+
+
+}
