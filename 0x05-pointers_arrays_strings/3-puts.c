77972fc411be6bde47a0720c735ebe15da409ff5
boampsremarks <boampsremarks@gmail.com>
2022-06-13 22:22:34 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string
+ *
+ * @str: input of str is a character
+ *
+ * Return: Always 0
+*/
+void _puts(char *str)
+{
+	puts(str);
+}
