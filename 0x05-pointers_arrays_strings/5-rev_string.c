0c738059d9bb9dc28f51bf2bff288976b372089f
boampsremarks <boampsremarks@gmail.com>
2022-06-13 22:32:17 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string in reverse
+ * @s: s is a string
+*/
+
+void rev_string(char *s)
+{
+	int len, count, end;
+	char beg;
+
+	len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	len--;
+
+	for (count = 0; count <= len / 2; count++)
+	{
+		beg = s[count];
+		end = s[len - count];
+		s[count] = end;
+		s[len - count] = beg;
+	}
+}
