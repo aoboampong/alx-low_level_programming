123c22cc40f3f897450ecec68f67ee34ec015cbc
boampsremarks <boampsremarks@gmail.com>
2022-06-27 03:56:51 +0000

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - this will locate character in a string
+ * @s: charr array string
+ * @c: char to locate
+ * Require: Write a function that locates a character
+ * in a string.
+ * Return: pointer to char c or NULL if char not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (*s != '\0', s++)
+	{
+		if (*s == c)
+			return (s);
+		else if (*(s + 1) == c)
+			return (s + 1);
+	}
+
+
+	return (s);
+}
