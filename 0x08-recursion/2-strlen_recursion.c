11ec30deb42362a79f3af83269e4cb11d784091f
boampsremarks <boampsremarks@gmail.com>
2022-07-03 12:34:22 +0000

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - recieves the length of string
+ * @s: points to a string
+ * Return: looper plus one
+ */
+
+int _strlen_recursion(char *s)
+looper = _strlen_recursion(s + 1)
+
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + looper);
+}
