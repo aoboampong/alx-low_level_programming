bd3f8a4eb0e890134979dfbdf9736af9b4e6b9f1
boampsremarks <boampsremarks@gmail.com>
2022-07-03 15:43:53 +0000

    Final commit

---
+++ 0x08-recursion/6-is-prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to evaluate
+ *
+ * Return: 1 for prime number, 0 for success
+ */
+
+int verifier(int n, int a);
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (verifier(n, n - 1));
+}
+
+/**
+ * verifier - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @a: performs calculation
+ *
+ * Return: 1 for prime number, 0 for success
+ */
+int verifier(int n, int a)
+{
+	if (a == 1)
+		return (1);
+	if (n % a == 0 && a > 0)
+		return (0);
+	return (verifier(n, a - 1));
+}
